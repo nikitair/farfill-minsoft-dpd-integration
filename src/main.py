142345a4605907bddfc2acbb81cc33c2e589044d
sakhaline <alexandra.ivva@gmail.com>
2024-03-07 13:44:47 +0200

    Start endpoints implementation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Request
 import uvicorn
 from logs.logging_config import logger
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 app = FastAPI()
@@ -12,6 +17,10 @@ def index():
     return {"message": "Hello World!"}
 
 
+@app.post("/api/Order/{id}/Shipments/CreateShipment")
+def create_shipment(request: Request, id: int):
+    pass
+
+
 if __name__ =="__main__":
     uvicorn.run(app=app, port=8000, host="0.0.0.0")
-
