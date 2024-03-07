d9a37381c9f4ef110afc49e85c308b4c7d546ddd
nkstlrv <stlrvnk@gmail.com>
2024-03-07 12:27:43 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+from logs.logging_config import logger
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    logger.info(f"{index.__name__} -- INDEX ENDPOINT TRIGGERED")
+    return {"message": "Hello World!"}
+
+
+if __name__ =="__main__":
+    uvicorn.run(app=app, port=8000, host="0.0.0.0", reload=True)
+
