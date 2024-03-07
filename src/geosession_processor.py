6c652e82fdef909c1f565198c5f1ae079c08bc78
sakhaline <alexandra.ivva@gmail.com>
2024-03-07 13:15:08 +0200

    Create geosession_processor.pu file

---
+++ src/geosession_processor.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def retrieve_geosession_token(api_key):
+    """
+    Function to retrieve a new Geosession token from the geolocation service's API.
+
+    Parameters:
+        api_key (str): Your API key for the geolocation service.
+
+    Returns:
+        str: The obtained Geosession token.
+    """
+    response = requests.get('https://geolocation-service.com/token',
+                            params={'key': api_key})
+
+    if response.status_code == 200:
+        return response.json().get('token')
+    else:
+        response.raise_for_status()
+
+
+if __name__ == '__main__':
+    api_key = 'YOUR_API_KEY'
+
+    # Retrieve Geosession token
+    geosession_token = retrieve_geosession_token(api_key)
+    print("Retrieved Geosession token:", geosession_token)
