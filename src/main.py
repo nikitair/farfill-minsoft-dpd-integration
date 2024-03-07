f1248ffb29eb1f689f060d519784eb9ffe126e61
ione <ivan.polyakoff2003@gmail.com>
2024-03-07 14:11:38 +0200

    Update endpoints implementation

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 import uvicorn
 from logs.logging_config import logger
 from dotenv import load_dotenv
+import json
 
 
 load_dotenv()
@@ -10,16 +11,30 @@ load_dotenv()
 
 app = FastAPI()
 
+def save_to_backup(data):
+    backup_file = "backup.json"
+    with open(backup_file, "a") as f:
+        json.dump(data, f)
+        f.write("\n")
 
 @app.get("/")
 def index():
     logger.info(f"{index.__name__} -- INDEX ENDPOINT TRIGGERED")
     return {"message": "Hello World!"}
 
+@app.get("/api/Order/{id}/Shipments")
+def get_shipment(request: Request, id: int):
+    shipments_data = request.json()
+    save_to_backup(shipments_data)
 
 @app.post("/api/Order/{id}/Shipments/CreateShipment")
 def create_shipment(request: Request, id: int):
     pass
+
+@app.post("/api/Order/{id}/Shipments/CancelShipment")
+def cancel_shipment(request: Request, id: int):
+    pass
+
 
 
 if __name__ =="__main__":
