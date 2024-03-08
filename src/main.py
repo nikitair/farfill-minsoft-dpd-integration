9f73e6f77839ce92c3afcc59887cfe1737480bff
nkstlrv <stlrvnk@gmail.com>
2024-03-08 12:56:39 +0200

    Add schemas for requests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request, HTTPException
 import uvicorn
 from logs.logging_config import logger
-from utils import save_to_backup
+# from utils import save_to_backup
+from schemas import CreateShipmentRequest, CancelShipmentRequest
 from views import create_shipment_view
+
 
 app = FastAPI()
 
@@ -15,7 +17,7 @@ async def index():
 
 # @app.get("/api/Order/{id}/Shipments")
 @app.post("/api/test/Order/Shipments/CreateShipment")
-async def create_shipment_test(request: Request):
+async def create_shipment_test(request: CreateShipmentRequest):
     logger.info(f"{create_shipment_test.__name__} -- CREATE SHIPMENT TEST ENDPOINT TRIGGERED")
 
     payload = None
@@ -56,7 +58,7 @@ async def create_shipment_test(request: Request):
 
 
 @app.post("/api/Order/Shipments/CreateShipment")
-async def create_shipment(request: Request):
+async def create_shipment(request: CreateShipmentRequest):
     logger.info(f"{create_shipment.__name__} -- CREATE SHIPMENT ENDPOINT TRIGGERED")
 
     payload = None
@@ -77,7 +79,7 @@ async def create_shipment(request: Request):
 
 
 @app.delete("/api/test/mintsoft/live/CancelShipment")
-async def cancel_shipment_test(request: Request):
+async def cancel_shipment_test(request: CancelShipmentRequest):
     logger.info(f"{cancel_shipment.__name__} -- CANCEL SHIPMENT TEST ENDPOINT TRIGGERED")
 
     payload = None
@@ -98,7 +100,7 @@ async def cancel_shipment_test(request: Request):
 
 
 @app.delete("/api/mintsoft/live/CancelShipment")
-async def cancel_shipment(request: Request):
+async def cancel_shipment(request: CancelShipmentRequest):
     logger.info(f"{cancel_shipment.__name__} -- CANCEL SHIPMENT ENDPOINT TRIGGERED")
 
     payload = None
--- src/schemas.py
+++ src/schemas.py
@@ -1,3 +1,91 @@
 from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, PositiveInt, Float, Decimal
 
 
+class CreateShipmentRequest(BaseModel):
+    AccountNo: str
+    Password: str
+    ShipmentId: str
+    ServiceName: str
+    ServiceCode: str
+    DeliveryNotes: str
+    Client: str
+    Warehouse: str
+    OrderNumber: str
+    ExternalOrderReference: str
+    Channel: str
+
+    class ShipFrom(BaseModel):
+        Email: EmailStr
+        Phone: str
+        Name: str
+        AddressLine1: str
+        AddressLine2: str | None = None
+        AddressLine3: str | None = None
+
+        Town: str
+        County: str
+        PostCode: str
+        CountryCode: str
+        VATNumber: str | None = None
+        EORINumber: str | None = None
+        IOSSNumber: str | None = None
+
+    class ShipTo(BaseModel):
+        Email: EmailStr
+        Phone: str
+        Name: str
+        AddressLine1: str
+        AddressLine2: str | None = None
+        AddressLine3: str | None = None
+        Town: str
+        County: str
+        PostCode: str
+        CountryCode: str
+        VATNumber: str | None = None
+        EORINumber: str | None = None
+
+    ShipFrom: ShipFrom
+    ShipTo: ShipTo
+
+    class Parcel(BaseModel):
+        ParcelNo: PositiveInt
+        UnitOfLength: str
+        Length: Float
+        Width: Float
+        Height: Float
+        UnitOfWeight: str
+        Weight: Float
+
+        class Cost(BaseModel):
+            Currency: str
+            Amount: Decimal
+
+        Cost: Cost
+
+        class ParcelItem(BaseModel):
+            Title: str
+            SKU: str
+            Quantity: PositiveInt
+            UnitWeight: Float
+
+            class UnitPrice(BaseModel):
+                Currency: str
+                Amount: Decimal
+
+            UnitPrice: UnitPrice
+            CommodityCode: str
+            CustomsDescription: str
+            CountryOfManufacture: str
+
+        Parcels: list[Parcel]
+
+    Parcels: list[Parcel]
+
+
+
+class CancelShipmentRequest(BaseModel):
+    AccountNo: str
+    Password: str
+    TrackingNumber: str
+    Comment: str | None = None
