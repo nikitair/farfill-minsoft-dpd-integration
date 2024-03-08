07fe24f751b7d71d4f659ff479dbc8e5c2b2f0d6
nkstlrv <stlrvnk@gmail.com>
2024-03-08 13:14:32 +0200

    Remove schemas [TEMP]

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, HTTPException
 import uvicorn
 from logs.logging_config import logger
 # from utils import save_to_backup
-from schemas import CreateShipmentRequest, CancelShipmentRequest
+# from schemas import CreateShipmentRequest, CancelShipmentRequest
 from views import create_shipment_view
 
 
@@ -17,13 +17,13 @@ async def index():
 
 # @app.get("/api/Order/{id}/Shipments")
 @app.post("/api/test/Order/Shipments/CreateShipment")
-async def create_shipment_test(request: CreateShipmentRequest):
+async def create_shipment_test(request: Request):
     logger.info(f"{create_shipment_test.__name__} -- CREATE SHIPMENT TEST ENDPOINT TRIGGERED")
 
     payload = None
     
     try:
-        payload = dict(request)
+        payload = await request.json()
         logger.info(f"{create_shipment_test.__name__} -- RECEIVED PAYLOAD - {payload}")
     except Exception:
         logger.exception(f"{create_shipment_test.__name__} -- ! BAD PAYLOAD ERROR")
@@ -58,13 +58,13 @@ async def create_shipment_test(request: CreateShipmentRequest):
 
 
 @app.post("/api/Order/Shipments/CreateShipment")
-async def create_shipment(request: CreateShipmentRequest):
+async def create_shipment(request: Request):
     logger.info(f"{create_shipment.__name__} -- CREATE SHIPMENT ENDPOINT TRIGGERED")
 
     payload = None
     
     try:
-        payload = dict(request)
+        payload = await request.json()
         logger.info(f"{create_shipment.__name__} -- RECEIVED PAYLOAD - {payload}")
     except Exception:
         logger.exception(f"{create_shipment.__name__} -- ! BAD PAYLOAD ERROR")
@@ -79,13 +79,13 @@ async def create_shipment(request: CreateShipmentRequest):
 
 
 @app.delete("/api/test/mintsoft/live/CancelShipment")
-async def cancel_shipment_test(request: CancelShipmentRequest):
+async def cancel_shipment_test(request: Request):
     logger.info(f"{cancel_shipment.__name__} -- CANCEL SHIPMENT TEST ENDPOINT TRIGGERED")
 
     payload = None
     
     try:
-        payload = dict(request)
+        payload = await request.json()
         logger.info(f"{cancel_shipment_test.__name__} -- RECEIVED PAYLOAD - {payload}")
     except Exception:
         logger.exception(f"{cancel_shipment_test.__name__} -- ! BAD PAYLOAD ERROR")
@@ -100,13 +100,13 @@ async def cancel_shipment_test(request: CancelShipmentRequest):
 
 
 @app.delete("/api/mintsoft/live/CancelShipment")
-async def cancel_shipment(request: CancelShipmentRequest):
+async def cancel_shipment(request: Request):
     logger.info(f"{cancel_shipment.__name__} -- CANCEL SHIPMENT ENDPOINT TRIGGERED")
 
     payload = None
     
     try:
-        payload = dict(request)
+        payload = await request.json()
         logger.info(f"{cancel_shipment.__name__} -- RECEIVED PAYLOAD - {payload}")
     except Exception:
         logger.exception(f"{cancel_shipment.__name__} -- ! BAD PAYLOAD ERROR")
