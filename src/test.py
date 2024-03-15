fee4c6ad30f7168126fb482b475e591e983d2dbc
ione <ivan.polyakoff2003@gmail.com>
2024-03-15 10:40:44 +0200

    update requirements.txt

--- src/test.py
+++ src/test.py
@@ -1,19 +1,21 @@
-from html2image import Html2Image
 import requests
-import img2pdf
 import base64
+import img2pdf
+from html2image import Html2Image
 
+# URL запиту до API для отримання етикетки
 label_endpoint = f"https://api.dpd.co.uk/shipping/shipment/1126684719/label/"
+
+# Заголовки для запиту
 label_headers = {
     "Accept": "text/html",
     "GeoSession": "f8a29d27-c2c6-4c11-afda-9177fcb22290",
     "GeoClient": "account/118990"
 }
 
+# Виконуємо запит
 response = requests.get(label_endpoint, headers=label_headers)
 response_text = response.text
-
-
 
 htmlimg = Html2Image()
 htmlimg.screenshot(html_str=response_text, save_as='label.png')
@@ -28,7 +30,7 @@ except FileNotFoundError:
     exit()
 
 
-image_base64 = base64.b64encode(img_data).decode('utf-8')
+LabelAsBase64 = base64.b64encode(img_data).decode('utf-8')
 
 
 pdf_path = 'label.pdf'
@@ -39,6 +41,8 @@ with open(pdf_path, 'wb') as pdf_file:
 with open(pdf_path, 'rb') as pdf_file:
     pdf_data = pdf_file.read()
 
-pdf_base64 = base64.b64encode(pdf_data).decode('utf-8')
+CustomsPDFDocumentAsBase64 = base64.b64encode(pdf_data).decode('utf-8')
+
+
 
 
