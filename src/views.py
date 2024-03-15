1f2748c89d765e52ad7674d9bfa8997828794fcd
ione <ivan.polyakoff2003@gmail.com>
2024-03-15 18:25:25 +0200

    fixed mapping

--- src/views.py
+++ src/views.py
@@ -206,10 +206,9 @@ def create_shipment_view(payload):
     # ship_from_address3 = payload["ShipFrom"]["AddressLine3"]
     ship_from_town = payload["ShipFrom"]["Town"]
     ship_from_county = payload["ShipFrom"]["County"]
-    try:
-        ship_from_postcode = payload["ShipFrom"]["PostCode"]
-    except KeyError:
-        ship_from_postcode = "CV34FR"
+
+    # ship_from_postcode = payload["ShipFrom"]["PostCode"]
+    ship_from_postcode = "CV34FR"
 
     ship_from_country_code = payload["ShipFrom"]["CountryCode"]
     # ship_from_vat_number = payload["ShipFrom"]["VATNumber"]
@@ -225,10 +224,10 @@ def create_shipment_view(payload):
     # ship_to_address3 = payload["ShipTo"]["AddressLine3"]
     ship_to_town = payload["ShipTo"]["Town"]
     ship_to_county = payload["ShipTo"]["County"]
-    try:
-        ship_to_postcode = payload["ShipTo"]["PostCode"]
-    except KeyError:
-        ship_from_postcode = "10001"
+
+        # ship_to_postcode = payload["ShipTo"]["PostCode"]
+
+    ship_from_postcode = "10001"
     ship_to_country_code = payload["ShipTo"]["CountryCode"]
     # ship_to_vat_number = payload["ShipTo"]["VATNumber"]
     # ship_to_eori_number = payload["ShipTo"]["EORINumber"]
