b1ed27fd649b36c378dfde45df3aa1b84e1bb29b
nkstlrv <stlrvnk@gmail.com>
2024-03-15 15:33:43 +0200

    Remove zoom and margins

--- src/views.py
+++ src/views.py
@@ -335,11 +335,11 @@ def send_to_mintsoft(response_text, order_number):
     # Convert HTML to PDF
     pdfkit.from_string(response_text, pdf_file, options = {
         'page-size': 'A7',  # Set the page size
-        'margin-top': '0.75in',
-        'margin-right': '0.75in',
-        'margin-bottom': '0.75in',
-        'margin-left': '0.75in',
-        'zoom': 6.0,  # Increase the zoom factor to make images larger
+        'margin-top': '0.01in',
+        'margin-right': '0.01in',
+        'margin-bottom': '0.01in',
+        'margin-left': '0.01in',
+        'zoom': 2.0,  # Increase the zoom factor to make images larger
     })
 
     # Path where you want to save the PNG images
